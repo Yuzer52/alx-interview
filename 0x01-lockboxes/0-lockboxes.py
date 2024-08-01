18429aee3be9ad939cd8422a24d7b2e1e5504a81
Yuzer52 <yuzer5252@gmail.com>
2024-08-01 14:39:23 -0700

    the first steps

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -10,21 +10,27 @@ def canUnlockAll(boxes):
     based on keys that can be attained.
     Solution to the lockboxes problem
     """
-    if (type(boxes)) is not list:
-        return False
-    elif (len(boxes)) == 0:
+    # Check if the input is a list
+    if type(boxes) is not list:
         return False
 
-    unlocked = [False] * (len(boxes))
-    unlocked[0] = True
-    keys = [0]
+    # Check if the list of boxes is empty
+    elif len(boxes) == 0:
+        return False
 
+    # Initialize a list to keep track of unlocked boxes
+    unlocked = [False] * len(boxes)
+    unlocked[0] = True  # The first box is always unlocked
+    keys = [0]  # Start with the keys in the first box
+
+    # Use keys to unlock boxes iteratively
     while keys:
         current_key = keys.pop()
         for key in boxes[current_key]:
-            if key < len(boxes) and not unlocked[key]:
+            if key < len(boxes) and not unlocked[key]:  # Check if key is valid and box is not yet unlocked
                 unlocked[key] = True
-                keys.append(key)
+                keys.append(key)  # Add new key to the list of keys to process
 
+    # Check if all boxes are unlocked
     return all(unlocked)
 
