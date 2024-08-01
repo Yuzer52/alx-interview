8eb5dac29aa3068abefde6899c46ec580d9be218
Yuzer52 <yuzer5252@gmail.com>
2024-08-01 14:13:16 -0700

    for the forth time

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
-
 def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be opened.
+
+    Args:
+        boxes (list of list of int): A list of boxes, each containing a list of keys.
+
+    Returns:
+        bool: True if all boxes can be opened, else False.
+    """
     n = len(boxes)
     unlocked = [False] * n
     unlocked[0] = True  # The first box is unlocked
@@ -10,10 +18,11 @@ def canUnlockAll(boxes):
     while keys:
         current_key = keys.pop()
         for key in boxes[current_key]:
-            if key < n and not unlocked[key]:  
+            if key < n and not unlocked[key]:  # Ensure the key is valid and the box is not yet unlocked
                 unlocked[key] = True
                 keys.append(key)
     
     return all(unlocked)
+
 
 
