5796ef3184f53912732f3730f48692227eaceeac
Yuzer52 <yuzer5252@gmail.com>
2024-08-01 14:41:19 -0700

    the second check

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,36 +1,33 @@
 #!/usr/bin/python3
 """
-Solution to lockboxes problem
+This is a module that provides a function for determining if all
+boxes in a given list can be opened.
 """
 
 
 def canUnlockAll(boxes):
     """
-    Determines whether a series of locked boxes can be opened
-    based on keys that can be attained.
-    Solution to the lockboxes problem
+    This function takes a list of lists and returns a boolean indicating
+        whether all boxes in the list can be opened. A key with the same
+        number as a box opens that box. You can assume all keys will be
+        positive integers. There can be keys that do not have boxes.
+        The first box boxes[0] is unlocked.
+
+    Parameters:
+    boxes (List[List[int]]): The list of lists representing the boxes
+        and their keys.
+
+    Returns:
+    bool: True if all boxes can be opened, else False.
     """
-    # Check if the input is a list
-    if type(boxes) is not list:
-        return False
-
-    # Check if the list of boxes is empty
-    elif len(boxes) == 0:
-        return False
-
-    # Initialize a list to keep track of unlocked boxes
-    unlocked = [False] * len(boxes)
-    unlocked[0] = True  # The first box is always unlocked
-    keys = [0]  # Start with the keys in the first box
-
-    # Use keys to unlock boxes iteratively
-    while keys:
-        current_key = keys.pop()
-        for key in boxes[current_key]:
-            if key < len(boxes) and not unlocked[key]:  # Check if key is valid and box is not yet unlocked
-                unlocked[key] = True
-                keys.append(key)  # Add new key to the list of keys to process
-
-    # Check if all boxes are unlocked
-    return all(unlocked)
-
+    n = len(boxes)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if boxIdx not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+            seen_boxes.add(boxIdx)
+    return n == len(seen_boxes)
