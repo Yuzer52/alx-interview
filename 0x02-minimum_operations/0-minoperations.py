28d6934aa1970baf8f21433b395500941829c15d
Yuzer52 <yuzer5252@gmail.com>
2024-08-08 05:50:13 -0700

    commited sucess

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,37 @@
 #!/usr/bin/python3
 """ Minimum Operations
-    """
-
+    This module defines a function to calculate the minimum
+    number of operations needed to achieve exactly n 'H' characters
+    in a text file using only 'Copy All' and 'Paste' operations.
+"""
 
 def minOperations(n: int) -> int:
-    """ Minimum Operations needed to get n H characters """
-    next = 'H'
-    body = 'H'
-    op = 0
-    while (len(body) < n):
-        if n % len(body) == 0:
-            op += 2
-            next = body
-            body += body
+    """ 
+    Calculate the minimum operations needed to result in exactly n 'H' characters.
+    
+    Args:
+        n (int): The target number of 'H' characters.
+    
+    Returns:
+        int: The minimum number of operations needed to achieve n 'H' characters,
+             or 0 if it is impossible to achieve exactly n 'H' characters.
+    """
+    
+    copied = 'H'
+    current = 'H'
+    operations = 0
+    
+    while len(current) < n:
+        if n % len(current) == 0:
+            operations += 2
+            copied = current
+            current += current
         else:
-            op += 1
-            body += next
-    if len(body) != n:
+            operations += 1
+            current += copied
+    
+    if len(current) != n:
         return 0
-    return op
+    
+    return operations
+
